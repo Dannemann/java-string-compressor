7bf3e168005a9255acdc2d313d8abd93a2d8626e
Jean Dannemann Carone <jean.carone@gmail.com>
2025-04-21 02:04:56 -0300

    Test for odd length strings at FiveB.itAsciiCompressorTest.java

--- src/test/java/com/stringcompressor/FiveBitAsciiCompressorTest.java
+++ src/test/java/com/stringcompressor/FiveBitAsciiCompressorTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FiveBitAsciiCompressorTest {
 
 	@Test
-	public void validCustomCharsetTest() {
+	public void even() {
 		AsciiCompressor compressor = new FiveBitAsciiCompressor(DEFAULT_4BIT_CHARSET);
 
 		byte[] compressed = compressor.compress("0123456789;#-+.,,.+-#;9876543210".getBytes(US_ASCII));
@@ -26,5 +26,18 @@ public class FiveBitAsciiCompressorTest {
 		assertEquals("[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]", Arrays.toString(decompressed));
 	}
 
+
+	@Test
+	public void odd() {
+		AsciiCompressor compressor = new FiveBitAsciiCompressor(DEFAULT_4BIT_CHARSET);
+
+		byte[] compressed = compressor.compress("0123456789;#-+.,,.+-#;9876543210,".getBytes(US_ASCII));
+		System.out.println(Arrays.toString(compressed));
+//		assertEquals("[0, 68, 50, 20, -57, 66, 84, -74, 53, -49, 123, -102, -59, -87, 40, 57, -118, 65, -120, 32]", Arrays.toString(compressed));
+
+		byte[] decompressed = compressor.decompress(compressed);
+		System.out.println(Arrays.toString(decompressed));
+		assertEquals("[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 15]", Arrays.toString(decompressed));
+	}
 
 }
