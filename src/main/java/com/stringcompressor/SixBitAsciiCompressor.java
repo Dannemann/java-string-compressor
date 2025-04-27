a9c1e1cd33b274eaaabe9e3e038479a505dddb9a
Jean Dannemann Carone <jean.carone@gmail.com>
2025-04-27 05:52:43 -0300

    SixBitAsciiCompressor working.

--- src/main/java/com/stringcompressor/SixBitAsciiCompressor.java
+++ src/main/java/com/stringcompressor/SixBitAsciiCompressor.java
@@ -17,6 +17,15 @@ public class SixBitAsciiCompressor extends AsciiCompressor {
 		'[', ']', '_', '{', '}'
 	};
 
+	public static final byte[] DEFAULT_6BIT_CHARSET_LOWERCASE = new byte[]{
+		' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/',
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+		':', ';', '<', '=', '>', '?', '@',
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+		'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		'[', ']', '_', '{', '}'
+	};
+
 	public SixBitAsciiCompressor() {
 		super(DEFAULT_6BIT_CHARSET);
 	}
--- src/test/java/com/stringcompressor/SixBitAsciiCompressorTest.java
+++ src/test/java/com/stringcompressor/SixBitAsciiCompressorTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-import static com.stringcompressor.FiveBitAsciiCompressor.DEFAULT_5BIT_CHARSET;
 import static com.stringcompressor.SixBitAsciiCompressor.DEFAULT_6BIT_CHARSET;
 import static java.nio.charset.StandardCharsets.US_ASCII;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,65 +16,65 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  */
 public class SixBitAsciiCompressorTest extends BaseTest {
 
-//	@Test
-//	public void validCustomCharsetTest() {
-//		byte[] customSupportedCharset = new byte[]{
-//			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-//			16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
-//		new FiveBitAsciiCompressor(customSupportedCharset);
-//	}
-//
-//	@Test
-//	public void excessCustomCharsetTest() {
-//		byte[] customSupportedCharset = new byte[]{
-//			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-//			16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32};
-//		CharacterNotSupportedException e = assertThrows(
-//			CharacterNotSupportedException.class, () -> new FiveBitAsciiCompressor(customSupportedCharset));
-//		assertEquals("5-bit compressor requires a set of exactly 32 characters. Currently 33.", e.getMessage());
-//	}
-//
-//	@Test
-//	public void missingCustomCharsetTest() {
-//		byte[] customSupportedCharset = new byte[]{
-//			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-//			16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
-//		CharacterNotSupportedException e = assertThrows(
-//			CharacterNotSupportedException.class, () -> new FiveBitAsciiCompressor(customSupportedCharset));
-//		assertEquals("5-bit compressor requires a set of exactly 32 characters. Currently 31.", e.getMessage());
-//	}
-//
-//	@Test
-//	public void invalidCustomCharsetTest() {
-//		byte[] customSupportedCharset = new byte[]{
-//			-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-//			16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
-//		CharacterNotSupportedException e = assertThrows(
-//			CharacterNotSupportedException.class, () -> new FiveBitAsciiCompressor(customSupportedCharset));
-//		assertEquals("Invalid character found in the custom supported charset: '\uFFFF' (code point -1)", e.getMessage());
-//	}
-//
-//	@Test
-//	public void notAsciiCharCompressTest() {
-//		AsciiCompressor compressor = new FiveBitAsciiCompressor(true);
-//		CharacterNotSupportedException e = assertThrows(
-//			CharacterNotSupportedException.class, () -> compressor.compress(new byte[]{(byte) 'Ç'}));
-//		assertEquals("Only ASCII characters are supported. Invalid 'ￇ' (code point -57) in \"�\"", e.getMessage());
-//	}
-//
-//	@Test
-//	public void invalidCharCompressTest() {
-//		AsciiCompressor compressor = new FiveBitAsciiCompressor(true);
-//		CharacterNotSupportedException e = assertThrows(
-//			CharacterNotSupportedException.class, () -> compressor.compress(new byte[]{'9'}));
-//		assertEquals("Character '9' (code point 57) is not defined in the supported characters array. String: \"9\"", e.getMessage());
-//	}
+	@Test
+	public void validCustomCharsetTest() {
+		byte[] customSupportedCharset = new byte[]{
+			0, 1, 2, 3, 4, 6, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 16, 17, 18, 19, 20, 21, 22, 23, 24, 26, 26, 27, 28, 29, 30, 31,
+			32, 33, 34, 36, 36, 37, 38, 39, 40, 41, 42, 43, 44, 46, 46, 47, 48, 49, 50, 51, 52, 53, 54, 56, 56, 57, 58, 59, 60, 61, 62, 63};
+		new SixBitAsciiCompressor(customSupportedCharset);
+	}
+
+	@Test
+	public void excessCustomCharsetTest() {
+		byte[] customSupportedCharset = new byte[]{
+			0, 1, 2, 3, 4, 6, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 16, 17, 18, 19, 20, 21, 22, 23, 24, 26, 26, 27, 28, 29, 30, 31,
+			32, 33, 34, 36, 36, 37, 38, 39, 40, 41, 42, 43, 44, 46, 46, 47, 48, 49, 50, 51, 52, 53, 54, 56, 56, 57, 58, 59, 60, 61, 62, 63, 64};
+		CharacterNotSupportedException e = assertThrows(
+			CharacterNotSupportedException.class, () -> new SixBitAsciiCompressor(customSupportedCharset));
+		assertEquals("6-bit compressor requires a set of exactly 64 characters. Currently 65.", e.getMessage());
+	}
+
+	@Test
+	public void missingCustomCharsetTest() {
+		byte[] customSupportedCharset = new byte[]{
+			0, 1, 2, 3, 4, 6, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 16, 17, 18, 19, 20, 21, 22, 23, 24, 26, 26, 27, 28, 29, 30, 31,
+			32, 33, 34, 36, 36, 37, 38, 39, 40, 41, 42, 43, 44, 46, 46, 47, 48, 49, 50, 51, 52, 53, 54, 56, 56, 57, 58, 59, 60, 61, 62};
+		CharacterNotSupportedException e = assertThrows(
+			CharacterNotSupportedException.class, () -> new SixBitAsciiCompressor(customSupportedCharset));
+		assertEquals("6-bit compressor requires a set of exactly 64 characters. Currently 63.", e.getMessage());
+	}
+
+	@Test
+	public void invalidCustomCharsetTest() {
+		byte[] customSupportedCharset = new byte[]{
+			-1, 0, 1, 2, 3, 4, 6, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 16, 17, 18, 19, 20, 21, 22, 23, 24, 26, 26, 27, 28, 29, 30, 31,
+			32, 33, 34, 36, 36, 37, 38, 39, 40, 41, 42, 43, 44, 46, 46, 47, 48, 49, 50, 51, 52, 53, 54, 56, 56, 57, 58, 59, 60, 61, 62};
+		CharacterNotSupportedException e = assertThrows(
+			CharacterNotSupportedException.class, () -> new SixBitAsciiCompressor(customSupportedCharset));
+		assertEquals("Invalid character found in the custom supported charset: '\uFFFF' (code point -1)", e.getMessage());
+	}
+
+	@Test
+	public void notAsciiCharCompressTest() {
+		AsciiCompressor compressor = new SixBitAsciiCompressor(true);
+		CharacterNotSupportedException e = assertThrows(
+			CharacterNotSupportedException.class, () -> compressor.compress(new byte[]{(byte) 'Ç'}));
+		assertEquals("Only ASCII characters are supported. Invalid 'ￇ' (code point -57) in \"�\"", e.getMessage());
+	}
+
+	@Test
+	public void invalidCharCompressTest() {
+		AsciiCompressor compressor = new SixBitAsciiCompressor(true);
+		CharacterNotSupportedException e = assertThrows(
+			CharacterNotSupportedException.class, () -> compressor.compress(new byte[]{'|'}));
+		assertEquals("Character '|' (code point 124) is not defined in the supported characters array. String: \"|\"", e.getMessage());
+	}
 
 	@Test
 	public void compressDecompressTest() {
 		AsciiCompressor compressor = new SixBitAsciiCompressor();
-		for (int length = 0; length <= 1000; length++)
-			for (int i = 0; i <= 10000; i++) {
+		for (int length = 0; length <= 500; length++)
+			for (int i = 0; i <= 5000; i++) {
 				String str = createRandomString(length, DEFAULT_6BIT_CHARSET);
 				byte[] compressed = compressor.compress(str.getBytes(US_ASCII));
 				byte[] decompressed = compressor.decompress(compressed);
@@ -83,34 +82,34 @@ public class SixBitAsciiCompressorTest extends BaseTest {
 			}
 	}
 
-//	@Test
-//	public void ignoreInvalidCharTest() {
-//		AsciiCompressor compressor = new FiveBitAsciiCompressor();
-//		byte[] compressed = compressor.compress(new byte[]{'A', (byte) 'Ç', 'B', 'C'});
-//		byte[] decompressed = compressor.decompress(compressed);
-//		assertEquals("AGBC", new String(decompressed, US_ASCII));
-//	}
-//
-//	@Test
-//	public void ignoreInvalidCharsTest() {
-//		AsciiCompressor compressor = new FiveBitAsciiCompressor();
-//		for (int i = 0; i < 3000; i++)
-//			for (int asciiCode = 0; asciiCode < 128; asciiCode++) {
-//				byte[] input = new byte[]{'A', (byte) asciiCode, 'B', 'C', 'D', (byte) 'Ç'};
-//				byte[] compressed = compressor.compress(input);
-//				byte[] decompressed = compressor.decompress(compressed);
-//				assertEquals(input.length, decompressed.length);
-//			}
-//	}
-//
-//	@Test
-//	public void compressionRateTest() {
-//		AsciiCompressor compressor = new FiveBitAsciiCompressor();
-//		int hundredMb = 100 * 1024 * 1024;
-//		byte[] input = new byte[hundredMb];
-//		Arrays.fill(input, (byte) 'A');
-//		byte[] compressed = compressor.compress(input);
-//		Assertions.assertEquals(62, compressed.length / 1024 / 1024); // 38% compression rate.
-//	}
+	@Test
+	public void ignoreInvalidCharTest() {
+		AsciiCompressor compressor = new SixBitAsciiCompressor();
+		byte[] compressed = compressor.compress(new byte[]{'A', (byte) 'Ç', 'B', 'C'});
+		byte[] decompressed = compressor.decompress(compressed);
+		assertEquals("AGBC", new String(decompressed, US_ASCII));
+	}
+
+	@Test
+	public void ignoreInvalidCharsTest() {
+		AsciiCompressor compressor = new SixBitAsciiCompressor();
+		for (int i = 0; i < 3000; i++)
+			for (int asciiCode = 0; asciiCode < 128; asciiCode++) {
+				byte[] input = new byte[]{'A', (byte) asciiCode, 'B', 'C', 'D', (byte) 'Ç'};
+				byte[] compressed = compressor.compress(input);
+				byte[] decompressed = compressor.decompress(compressed);
+				assertEquals(input.length, decompressed.length);
+			}
+	}
+
+	@Test
+	public void compressionRateTest() {
+		AsciiCompressor compressor = new SixBitAsciiCompressor();
+		int hundredMb = 100 * 1024 * 1024;
+		byte[] input = new byte[hundredMb];
+		Arrays.fill(input, (byte) 'A');
+		byte[] compressed = compressor.compress(input);
+		Assertions.assertEquals(75, compressed.length / 1024 / 1024); // 25% compression rate.
+	}
 
 }
