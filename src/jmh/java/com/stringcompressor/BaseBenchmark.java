0779c57205daef4b04439878a7afb0b6b484fc7e
Jean Dannemann Carone <jean.carone@gmail.com>
2025-04-28 05:01:26 -0300

    Tweaks.

--- src/jmh/java/com/stringcompressor/BaseBenchmark.java
+++ src/jmh/java/com/stringcompressor/BaseBenchmark.java
@@ -3,9 +3,9 @@ package com.stringcompressor;
 /**
  * @author Jean Dannemann Carone
  */
-public class BaseBenchmark {
+class BaseBenchmark {
 
-	public static byte[] generate10MbString(byte[] charset) {
+	static byte[] generate10MbString(byte[] charset) {
 		return BaseTest.generateRandomString(10 * 1024 * 1024, charset);
 	}
 
